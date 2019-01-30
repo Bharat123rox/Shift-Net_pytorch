2935a6a294fb40b111e95de541779540246b4d5d
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2019-01-30 12:08:14 +0800

    fix typo

--- models/shift_net/InnerCosFunction.py
+++ models/shift_net/InnerCosFunction.py
@@ -15,22 +15,14 @@ class InnerCosFunction(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        print('original former grad mean ', grad_output[:, 0:ctx.c//2, :, :].mean())
         with torch.enable_grad():
             input, target, mask = ctx.saved_tensors
             former = input.narrow(1, 0, ctx.c//2)
             former_in_mask = torch.mul(former, mask)
-            print(former_in_mask.requires_grad)
-            print(target.requires_grad)
             former_in_mask_clone = former_in_mask.clone().detach().requires_grad_(True)
             ctx.loss = ctx.criterion(former_in_mask_clone, target) * ctx.strength
             ctx.loss.backward()
-            print('now former grad mean ',former_in_mask_clone.grad.mean())
-            
-        print('now former grad mean ', grad_output[:, 0:ctx.c//2, :, :].mean())
-            
-        print()
-        assert 1==2
 
-        grad_output[:,0:ctx.c//2, :,:] += former_in_mask.grad
+        grad_output[:,0:ctx.c//2, :,:] += former_in_mask_clone.grad  
+            
         return grad_output, None, None, None, None
